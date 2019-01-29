0776e97f1007ce60d9723506e43d8b6c895064ea
ChingChang9 <chingtheprogrammer@icloud.com>
2019-01-29 01:02:40 -0700

    disabled sourcemaps

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  lintOnSave: undefined
+  lintOnSave: undefined,
+  baseUrl: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: false,
+  parallel: undefined,
+  css: undefined
 };
